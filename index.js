f31a322c4258d9ffe2a72ddd4ac3dafd050b9af9
aniketkrraj746 <aniketkrraj746@gmail.com>
2023-10-03 15:34:39 +0530

    index.js updated

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use("/api/v1", userRoutes);
 
 // static files
-app.use(express.static(path.join(__dirname,"../frontend/build")))
-app.get((req, res) => {
-  res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
-});
+// app.use(express.static(path.join(__dirname,"../frontend/build")))
+// app.get((req, res) => {
+//   res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
+// });
 
 // CORS Configuration
 app.listen(PORT, () => {
